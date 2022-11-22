ce5177c505815d1307b4ad3d50083b6bdfb3d4b5
anazhmetdin <anazhmetdin@gmail.com>
2022-11-22 23:25:19 +0200

    test image processing module

---
+++ src/tests/middleware/images/imagesSpec.ts
@@ -0,0 +1,142 @@
+import supertest from 'supertest'
+import app from '../../../index'
+import { unlink, stat, unlinkSync, accessSync, access } from 'fs'
+import path from 'path'
+
+const request = supertest(app)
+
+describe('Test images api', () => {
+    describe('Test images.mw', () => {
+        describe('Test paramsExist middleware', () => {
+            it('detects missing width param', async () => {
+                const responseProminse = request.get(
+                    '/api/images?height=400&filename=fjord'
+                )
+
+                responseProminse.catch((reason) => {})
+
+                const response = await responseProminse
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'missing parameter, must include width'
+                )
+            })
+
+            it('detects missing height param', async () => {
+                const response = await request.get(
+                    '/api/images?width=400&filename=fjord'
+                )
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'missing parameter, must include height'
+                )
+            })
+
+            it('detects missing filename param', async () => {
+                const response = await request.get(
+                    '/api/images?height=400&width=400'
+                )
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'missing parameter, must include filename'
+                )
+            })
+
+            it('detects extra params', async () => {
+                const response = await request.get(
+                    '/api/images?height=400&width=400&filename=fjord&extra'
+                )
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'wrong parameters: must only include filename, width, heigth'
+                )
+            })
+        })
+
+        describe('Test checkParamsValues middleware', () => {
+            it('detects invalid width values', async () => {
+                const response = await request.get(
+                    '/api/images?height=400&width=-400&filename=fjord'
+                )
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'invalid width value, must be a positive number'
+                )
+            })
+
+            it('detects invalid height values', async () => {
+                const response = await request.get(
+                    '/api/images?height=0&width=300&filename=fjord'
+                )
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'invalid height value, must be a positive number'
+                )
+            })
+
+            it('detects invalid filenmae values', async () => {
+                const response = await request.get(
+                    '/api/images?height=400&width=400&filename='
+                )
+                expect(response.status).toEqual(400)
+                expect(response.text).toEqual(
+                    'invalid filename value, must be a non-empty srting'
+                )
+            })
+        })
+
+        describe('Test checkFile middleware', () => {
+            it('detects filenames that does not exist', async () => {
+                const response = await request.get(
+                    '/api/images?height=400&width=400&filename=fjordd'
+                )
+                expect(response.status).toEqual(404)
+                expect(response.text).toEqual(
+                    "The requested image doesn't exist or can't be access currently"
+                )
+            })
+        })
+    })
+    describe('Test processing.mw', () => {
+        const thumpname = path.normalize(
+            `${__dirname}/../../../../thumps/fjord_400_400.jpg`
+        )
+
+        describe('Test resize middleware', () => {
+            it('resizes the image successfully', async () => {
+                // delete the processed image if exists
+                try {
+                    unlinkSync(thumpname)
+                } catch {}
+
+                const response = await request.get(
+                    '/api/images?height=400&width=400&filename=fjord'
+                )
+                expect(response.status).toEqual(200)
+            })
+        })
+
+        describe('Test checkCache middleware', () => {
+            it('sends chached image successfully', async () => {
+                // check the stats of the old processed image
+                stat(thumpname, async (err1, stats1) => {
+                    if (err1) {
+                        throw err1
+                    } else {
+                        // if the old processed image was found
+                        // request the same image again
+                        const response = await request.get(
+                            '/api/images?height=400&width=400&filename=fjord'
+                        )
+                        expect(response.status).toEqual(200)
+
+                        // check that the requested image wasn't modified after the second request
+                        stat(thumpname, (err2, stat2) => {
+                            expect(stats1.mtime).toEqual(stat2.mtime)
+                        })
+                    }
+                })
+            })
+        })
+    })
+})
