21933104487f93fcc849d8fcc0b4f978b78ecc38
anazhmetdin <anazhmetdin@gmail.com>
2022-11-22 21:25:10 +0200

    add caching functionality

--- src/api/images/images.api.ts
+++ src/api/images/images.api.ts
@@ -5,6 +5,6 @@ import sharp from '../../middleware/images/processing.mw'
 const imgsRoutes = Router()
 
 imgsRoutes.get('/', img.helloWorld)
-imgsRoutes.get('/images', img.paramsExist, img.checkParamsValues, img.checkFile, sharp.resize)
+imgsRoutes.get('/images', img.paramsExist, img.checkParamsValues, img.checkFile, sharp.checkCache, sharp.resize)
 
 export default imgsRoutes
--- src/middleware/images/processing.mw.ts
+++ src/middleware/images/processing.mw.ts
@@ -1,6 +1,26 @@
 import express from 'express'
 import sharp from 'sharp'
 import path from 'path'
+import { access } from 'fs'
+
+const checkCache = async (
+    req: express.Request,
+    res: express.Response,
+    next: Function
+) => {
+    
+    const width = parseInt(<string>req.query.width)
+    const height = parseInt(<string>req.query.height)
+    const thumpname =  path.normalize(`${__dirname}/../../../thumps/${req.query.filename}_${width}_${height}.jpg`);
+
+    access(thumpname, (err) => 
+    {
+        if (err)
+            next()
+        
+            res.sendFile(thumpname)
+    })    
+}
 
 const resize = async (
     req: express.Request,
@@ -10,17 +30,17 @@ const resize = async (
 
     const width = parseInt(<string>req.query.width)
     const height = parseInt(<string>req.query.height)
-    const filename = path.normalize(`${__dirname}/../../../images/${(<string>req.query.filename)}.jpg`);
-    const thumpname =  path.normalize(`${__dirname}/../../../thumps/${req.query.filename}_${width}_${height}.jpg`);
+    const filename = path.normalize(`${__dirname}/../../../images/${(<string>req.query.filename)}.jpg`)
+    const thumpname =  path.normalize(`${__dirname}/../../../thumps/${req.query.filename}_${width}_${height}.jpg`)
 
     sharp(filename)
         .resize(width, height)
         .toFile(thumpname, (err) => {
             if(err)
-                res.status(500).send("Internal server error, please try again later");
+                res.status(500).send("Internal server error, please try again later")
             else
-                res.sendFile(thumpname);
+                res.sendFile(thumpname)
         })
 }
 
-export default {resize}
+export default {checkCache, resize}
