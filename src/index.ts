de003210bbc1312cf25cc2c3e9afccbd28389c60
anazhmetdin <anazhmetdin@gmail.com>
2022-11-23 14:50:16 +0200

    fix prettier script

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT ?? 3000
 
 app.listen(port, () => {
-  console.log('Server listening on port', port)
+    console.log('Server listening on port', port)
 })
 
 app.use('/', apiRoutes)
